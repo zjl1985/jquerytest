d86dce829796c17d520ed5bca83fc6841499edd9
zhang <zjl198544@163.com>
2017-02-27 09:32:15 +0800

    finish

--- js/jquery-test.js
+++ js/jquery-test.js
@@ -3,12 +3,12 @@ var JqueryTest;
     var initTable = function() {
         $('table tbody').empty();
         var begin = moment();
-        // for (var i = 0; i < 300; i++) {
+        // for (var i = 0; i < 2000; i++) {
         //     $('table tbody').append('<tr><td scope="row">' + (i + 1) + '</td><td class="table-info"><fieldset class="form-group"><input type="checkbox"><label for="checkbox1"></label></fieldset></td><td>3</td><td> <input type="text" name="txt_heheda" disabled></td></tr>');
         // }
 
         var trs = '';
-        for (var i = 0; i < 300; i++) {
+        for (var i = 0; i < 2000; i++) {
             trs += '<tr><td scope="row">' + (i + 1) + '</td><td class="table-info"><fieldset class="form-group"><input type="checkbox"><label for="checkbox1"></label></fieldset></td><td>3</td><td> <input name="txt_heheda" type="text" disabled></td></tr>';
         }
         $('table tbody').append(trs);
@@ -23,7 +23,10 @@ var JqueryTest;
         // _.forEach($trs, function(tr) {
         //     $(tr).addClass('table-success');
         // });
-        $trs.addClass('table-success');
+        for (var i = 0; i < 2000; i++) {
+            $('table tbody tr:eq(' + i + ')').addClass('table-success');
+        }
+        // $trs.addClass('table-success');
         var end = moment();
         var diff = end.diff(begin);
         $('#p_log').append('<br>耗时：' + diff + '毫秒');
@@ -47,25 +50,25 @@ var JqueryTest;
     var changeInput = function() {
         var begin = moment();
 
-        var $inputs = $('table tbody tr input');
+        var $inputs = $('table input:text');
         _.forEach($inputs, function(item) {
             $(item).val(Math.random());
         });
 
         var end = moment();
         var diff = end.diff(begin);
+        $('#p_log').append('<br>耗时：' + $inputs.length + 'ge ');
         $('#p_log').append('<br>耗时：' + diff + '毫秒');
     }
 
     var getCheck = function() {
 
-        var length = $('table tbody input:checkbox:checked').length;
+        var length = $(':contains("Warning").col').length;
         var notselectlength = '怎么写？';
-        // var notselectlength = $('table tbody not(input:checkbox:checked)').length;
+        // var notselectlength = $('table tbody :not(input:checkbox:checked)').length;
         // var notselectlength = $('table tbody input:checkbox:not(checked)').length;
         // var notselectlength = $('table tbody input:checkbox:not(:checked)').length;
-        // var notselectlength = $('table tbody input:not(checked)').length;
-
+        var notselectlength = $('table tbody input:not(:checked)').length;
         $('#p_log').append('<br>选中：' + length + '个');
         $('#p_log').append('<br>没选中：' + notselectlength + '个');
     }
